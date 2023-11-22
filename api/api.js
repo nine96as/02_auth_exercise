6c89578fcb4b35ab8341f07737559e8761c103af
nine96as <ashrafsaid79@gmail.com>
2023-11-22 16:37:38 +0000

    Complete challenge tasks 1-3

---
+++ api/api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+
+const postRouter = require('./routers/post');
+const userRouter = require('./routers/user');
+
+const api = express();
+
+api.use(cors());
+api.use(express.json());
+api.use(logger('dev'));
+
+api.get('/', (req, res) => {
+  res.status(200).json({
+    name: 'Discretion',
+    description: 'Send and receive private messages.',
+    endpoints: [
+      'GET  /posts',
+      'GET  /posts/:id',
+      'POST  /posts',
+      'POST  /users/login',
+      'POST  /users/register',
+      'GET  /users/logout'
+    ]
+  });
+});
+
+api.use('/posts', postRouter);
+api.use('/users', userRouter);
+
+module.exports = api;
